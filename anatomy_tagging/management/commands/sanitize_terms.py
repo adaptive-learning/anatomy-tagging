dd1422c2e3548f806c42bec023ac8e57f52ff929
Vit Stanislav <slaweet@seznam.cz>
2015-10-19 10:50:39 +0200

    Check for duplicities in sanitize_terms command

--- anatomy_tagging/management/commands/sanitize_terms.py
+++ anatomy_tagging/management/commands/sanitize_terms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from anatomy_tagging.models import Term
+from anatomy_tagging.models import Term, Path
 import re
 
 
@@ -22,7 +22,9 @@ class Command(BaseCommand):
         }
         rev_subs = dict(zip(subs.values(), subs.keys()))
         pattern = re.compile(r'\b(' + '|'.join(rev_subs.keys()).replace('.', '\.') + r')')
+        ## print '\b(' + '|'.join(rev_subs.keys()).replace('.', '\.') + r')\b'
         count = 0
+        duplicate_count = 0
 
         for t in Term.objects.all().exclude(code__in=['no-practice', 'too-small']):
             name_la = t.name_la
@@ -34,4 +36,29 @@ class Command(BaseCommand):
                 t.name_la = name_la
                 t.save()
                 count += 1
+            # TODO merge if on more terms on the same image
+            terms = self.find_duplicate_terms(t)
+            if len(terms) > 0:
+                print t, '->', terms
+                print
+                duplicate_count += 1
         print count, 'terms updated'
+        print duplicate_count, 'duplicate terms found'
+
+    def find_duplicate_terms(self, t):
+        terms = Term.objects.filter(name_la=t.name_la).exclude(id=t.id)
+        ret = []
+        for term in terms:
+            if self.is_duplicate(term, t):
+                # print 'dup', term, t
+                ret.append(term)
+        return ret
+
+    def is_duplicate(self, t1, t2):
+        paths1 = Path.objects.filter(term=t1).select_related('image')
+        images1 = [p.image for p in paths1]
+        paths2 = Path.objects.filter(term=t2, image__in=images1).select_related('image')
+        images2 = [p.image for p in paths2]
+        if len(paths2) > 0:
+            print list(set(images2))
+        return len(paths2) > 0
